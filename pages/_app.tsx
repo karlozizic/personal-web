09bd203a798c6c6d0d6e4a8682720aa098be5414
karlozizic <karlo.zizic1@gmail.com>
2023-02-03 12:22:40 +0100

    dodan logo i navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Navbar from '../components/Navbar.js'
 import {AnimatePresence} from "framer-motion"
-import Navbar2 from '../components/Navbar2.js'
+import Navbar from '../components/Navbar.js'
 
 function MyApp({ Component, pageProps, router}: AppProps) {
   return (
   <div className="py-10 px-10 lg:px-48 font-latoBold">
-    <Navbar2 />
+    <Navbar />
     <AnimatePresence initial = {false}>
       <Component key = {router.pathname} {...pageProps} />
     </AnimatePresence>
